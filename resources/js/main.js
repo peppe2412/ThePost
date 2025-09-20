38720be825d718cf5092808814cfcb2e80e952af
peppe2412 <peppeterrazzano79@gmail.com>
2025-09-20 23:19:05 +0200

    Front-end home

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,21 +1,55 @@
-document.addEventListener('DOMContentLoaded', () => {
-
+document.addEventListener("DOMContentLoaded", () => {
     function toggleDropdown() {
-        let dropdown = document.querySelector('#dropdown')
-        dropdown.classList.toggle('hidden')
+        let dropdown = document.querySelector("#dropdown");
+        dropdown.classList.toggle("hidden");
     }
 
-    window.addEventListener('click', (event) => {
-        let dropdown = document.querySelector('#dropdown')
-        let button = document.querySelector('#dropdown-btn')
-        if (!button.contains(event.target) && !dropdown.contains(event.target)) {
-            dropdown.classList.add('hidden')
+    window.addEventListener("click", (event) => {
+        let dropdown = document.querySelector("#dropdown");
+        let button = document.querySelector("#dropdown-btn");
+        if (
+            !button.contains(event.target) &&
+            !dropdown.contains(event.target)
+        ) {
+            dropdown.classList.add("hidden");
         }
-    })
+    });
 
     window.toggleDropdown = toggleDropdown;
 
+    let img_writer_effect = document.querySelector("#imgWriterHome");
 
-    
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 10) {
+            img_writer_effect.classList.add("effect-img-home-writer");
+        }
+    });
 
-})
+    let imgScroll = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHXyzV23BMz6dEBxDc5oTS6-z0Lm9bMQpbZg&s",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMdM9MEQ0ExL1PmInT3U5I8v63YXBEdoIT0Q&s",
+        "https://img.freepik.com/foto-gratuito/strumenti-sportivi_53876-138077.jpg",
+        "https://imageio.forbes.com/specials-images/imageserve/5d3703e2f1176b00089761a6/2020-Chevrolet-Corvette-Stingray/0x0.jpg?crop=4560,2565,x836,y799,safe&height=399&width=711&fit=bounds",
+        
+    ];
+
+    function scrollImage() {
+        let container = document.querySelector("#containerScroll");
+        container.innerHTML = "";
+
+        let imgUse = [];
+        for (let i = 0; i < 8; i++) {
+            imgUse.push(...imgScroll);
+        }
+
+
+        imgUse.forEach((src, index) => {
+            let img = document.createElement("img");
+            img.src = src;
+            img.alt = `${index}`;
+            container.appendChild(img);
+        });
+    }
+
+    window.onload = () => scrollImage();
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,23 +3,74 @@
 
 
     @if (session('message'))
-        <div class="bg-green-300">
-            {{ session('message') }}
+        <div class="flex justify-center py-8">
+            <div class="tw-message-span">
+                {{ session('message') }}
+            </div>
         </div>
     @endif
 
     @if (session('alert'))
-        <div class="bg-red-300">
-            {{ session('alert') }}
+        <div class="flex justify-center py-8">
+            <div class="tw-alert-span">
+                {{ session('alert') }}
+            </div>
         </div>
     @endif
 
-    <div class="container py-16">
+    <header class="container py-10">
         <div class="flex justify-center">
-            <div>
-                <h1 class="text-4xl text-center mb-5">Ultimi Annunci</h1>
+            <h1 class="text-[300px] font-bold">THE POST</h1>
+        </div>
+    </header>
+
+    <div class="py-16">
+        <div class="ml-8">
+            <div class="w-full">
+                <div class="flex justify-evenly">
+                    <div class="w-[500px]">
+                        <p class="text-3xl">Benvenuto su <strong>THE POST</strong>, la piattaforma pensata per chi
+                            scrive.
+                            Qui
+                            trovi
+                            strumenti, risorse e uno spazio dedicato alla condivisione delle tue opere. Che tu sia
+                            autore
+                            emergente
+                            o già affermato, il nostro obiettivo è valorizzare il tuo talento e offrirti visibilità
+                            verso un
+                            pubblico sempre più ampio.</p>
+                    </div>
+
+                    <img class="w-[500px] rounded-2xl" id="imgWriterHome"
+                        src="https://blog-cdn.reedsy.com/directories/admin/featured_image/335/how-to-become-a-writer-1b28eb.webp"
+                        alt="Writer">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="py-24 scroll">
+        <div id="containerScroll"></div>
+    </div>
+
+    <div class="flex justify-center">
+        <div class="w-[50%]">
+            <p class="text-2xl">Su <strong>THE POST</strong> diamo spazio a ogni voce. Politica,
+                economia, sport, musica,
+                tecnologia, motori e molto altro: tanti mondi diversi uniti dalla stessa passione per la scrittura. Qui
+                ogni
+                autore trova il proprio spazio per raccontare, analizzare e condividere.</p>
+        </div>
+    </div>
+
+
+    <div class="container py-24">
+        <div class="flex justify-center">
+            <div class="mt-10">
+                <h2 class="text-6xl text-center mb-5">Ultimi Annunci</h2>
                 @foreach ($articles as $article)
-                   <x-article-card :article="$article"/>
+                    <x-article-card :article="$article" />
                 @endforeach
             </div>
         </div>
