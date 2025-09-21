dac1072214ae1092b26b61964262521c146fa63b
peppe2412 <peppeterrazzano79@gmail.com>
2025-09-21 16:14:58 +0200

    Front-end careers page

--- resources/js/main.js
+++ resources/js/main.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let img_writer_effect = document.querySelector("#imgWriterHome");
 
     window.addEventListener("scroll", () => {
-        if (window.scrollY > 10) {
+        if (window.scrollY > 0) {
             img_writer_effect.classList.add("effect-img-home-writer");
         }
     });
@@ -30,31 +30,60 @@ document.addEventListener("DOMContentLoaded", () => {
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMdM9MEQ0ExL1PmInT3U5I8v63YXBEdoIT0Q&s",
         "https://img.freepik.com/foto-gratuito/strumenti-sportivi_53876-138077.jpg",
         "https://imageio.forbes.com/specials-images/imageserve/5d3703e2f1176b00089761a6/2020-Chevrolet-Corvette-Stingray/0x0.jpg?crop=4560,2565,x836,y799,safe&height=399&width=711&fit=bounds",
-        
     ];
 
     function ImagesScroll(repeat = 0) {
-    let container = document.querySelector('#containerScroll')
-    container.innerHTML = ''
+        let container = document.querySelector("#containerScroll");
+        container.innerHTML = "";
 
-    let imagesUse = []
-    for (let i = 0; i < repeat; i++){
-        imagesUse = imagesUse.concat(imgScroll)
+        let imagesUse = [];
+        for (let i = 0; i < repeat; i++) {
+            imagesUse = imagesUse.concat(imgScroll);
+        }
+
+        imagesUse.forEach((src, index) => {
+            let image = document.createElement("img");
+            image.src = src;
+            image.alt = `${index}`;
+            container.appendChild(image);
+        });
+
+        container.addEventListener("scroll", () => {
+            if (container.scrollLeft >= container.scrollWidth / 2) {
+                container.scrollLeft = 0;
+            }
+        });
     }
 
-    imagesUse.forEach((src, index) => {
-        let image = document.createElement('img')
-        image.src = src
-        image.alt = `${index}`
-        container.appendChild(image)
+    window.onload = () => ImagesScroll(4);
+
+    let imgCareers = document.querySelectorAll(".careers-img");
+
+    let observer = new IntersectionObserver((entries)=>{
+        entries.forEach(entry => {
+            if(entry.isIntersecting){
+                entry.target.classList.add('effect-careers-img')
+                entry.target.classList.remove('opacity-0')
+            }
+        })
+    }, {
+        threshold : 0.2
     })
 
-    container.addEventListener('scroll', () => {
-        if (container.scrollLeft >= container.scrollWidth / 2) {
-            container.scrollLeft = 0
+    imgCareers.forEach(imgCar => observer.observe(imgCar))
+
+    let formCareersLink = document.querySelector('#link-form-careers')
+    let formCareers = document.querySelector('#careers-form')
+    
+    window.addEventListener('scroll', ()=>{
+        let getBound = formCareers.getBoundingClientRect()
+
+        if(getBound.bottom > 0 && getBound.top < window.innerHeight){
+            formCareersLink.classList.add('opacity-0')
+        }else{
+            formCareersLink.classList.remove('opacity-0')
         }
     })
-}
+    
 
-window.onload = () => ImagesScroll(16)
 });
--- resources/views/careers.blade.php
+++ resources/views/careers.blade.php
@@ -1,20 +1,89 @@
 <x-layout>
 
+    <div class="ml-5" id="link-form-careers">
+        <a class="link-form-careers text-lg bg-sky-300 rounded" href="#careers-form">Compila il form <span class="mx-2"><svg
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+                </svg>
+            </span>
+        </a>
+    </div>
 
     <header class="flex justify-center py-16">
         <h1 class="text-6xl">Lavora con noi</h1>
     </header>
 
-    <div class="container">
-        <div class="text-center mb-5">
-            <h2 class="text-2xl">Compila il Form</h2>
+    <div class="flex justify-center py-16">
+        <div class="w-[50%]">
+            <p class="text-center text-3xl">Vuoi entrare a far parte del nostro progetto? Cerchiamo appassionati di
+                scrittura, persone con voglia di mettersi in gioco e contribuire alla crescita della community. Che tu
+                voglia scrivere articoli, aiutare nella gestione o occuparti delle revisioni, qui troverai il tuo spazio
+            </p>
+        </div>
+    </div>
+
+    <div class="container py-16">
+        <h2 class="text-4xl text-center mb-10">
+            Admin
+        </h2>
+        <div class="flex">
+            <img class="tw-img-careers opacity-0 careers-img"
+                src="https://media.istockphoto.com/id/531792312/photo/office-work.jpg?s=612x612&w=0&k=20&c=F2XAB0GE9kV6d140abYeHcq6kciYa5wsYkn8K0P7cuY="
+                alt="Admin">
+            <div class="w-full flex items-center px-10">
+                <p class="text-2xl">Vuoi contribuire alla crescita del progetto anche dietro le quinte? Come admin avrai
+                    un ruolo
+                    fondamentale nell'organizzazione e nella gestione del sito. Dall’approvazione dei contenuti al
+                    supporto
+                    dei writer, sarai parte integrante del cuore della piattaforma.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-16">
+        <h2 class="text-4xl text-center mb-10">
+            Revisor
+        </h2>
+        <div class="flex">
+            <div class="w-full flex items-center px-10">
+                <p class="text-2xl">Precisione e attenzione ai dettagli sono il tuo punto di forza? Allora diventa
+                    revisore. Ti occuperai di controllare e migliorare i testi dei writer, garantendo qualità, chiarezza
+                    e correttezza dei contenuti pubblicati</p>
+            </div>
+            <img class="tw-img-careers opacity-0 careers-img"
+                src="https://img.freepik.com/foto-premium/computer-portatile-e-donna-che-digitano-ragioniere-e-lavorano-su-un-progetto-internet-nel-posto-di-lavoro-in-ufficio-computer-revisore-e-felice-professionista-femminile-che-scrive-rapporto-o-ricerca-via-e-mail-per-la-contabilita_590464-190829.jpg"
+                alt="Revisor">
+        </div>
+    </div>
+
+    <div class="container py-16">
+        <h2 class="text-4xl text-center mb-10">
+            Writer
+        </h2>
+        <div class="flex">
+            <img class="tw-img-careers opacity-0 careers-img"
+                src="https://t4.ftcdn.net/jpg/03/70/74/29/360_F_370742959_oafsXZ5nJ0gneXindh0pk6VE5lbtpfBT.jpg"
+                alt="Writer">
+            <div class="w-full flex items-center px-10">
+                <p class="text-2xl">Precisione e attenzione ai dettagli sono il tuo punto di forza? Allora diventa
+                    revisore. Ti occuperai di controllare e migliorare i testi dei writer, garantendo qualità, chiarezza
+                    e correttezza dei contenuti pubblicati</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div id="careers-form" class="container py-24">
+        <div class="text-center mb-10">
+            <h2 class="text-5xl">Compila il Form</h2>
         </div>
         <div class="flex justify-center">
-            <form action="{{ route('careers-submit') }}" method="POST">
+            <form action="{{ route('careers-submit') }}" method="POST" class="tw-form-careers">
                 @csrf
                 <div class="mb-5">
-                    <label for="role">In cosa ti stai candidando?</label>
-                    <select name="role" id="role">
+                    <label class="tw-label-careers" for="role">In cosa ti stai candidando?</label>
+                    <select class="border border-gray-400 w-full rounded p-2" name="role" id="role">
                         <option selected disabled>Seleziona il ruolo</option>
                         @if (!Auth::user()->is_admin)
                             <option value="admin">Amministratore</option>
@@ -31,20 +100,22 @@
                     @enderror
                 </div>
                 <div class="mb-5">
-                    <label for="email">Email</label>
-                    <input name="email" type="email">
+                    <label class="tw-label-careers" for="email">Email</label>
+                    <input class="border border-gray-400 w-full p-2 rounded" name="email" type="email">
                     @error('email')
                         <span class="tw-form-span-message">{{ $message }}</span>
                     @enderror
                 </div>
                 <div class="mb-5">
-                    <label for="message">Messaggio</label>
-                    <textarea name="message" id="message"></textarea>
+                    <label class="tw-label-careers" for="message">Messaggio</label>
+                    <textarea class="border border-gray-400 w-full p-2 rounded" name="message" id="message"></textarea>
                     @error('message')
                         <span class="tw-form-span-message">{{ $message }}</span>
                     @enderror
                 </div>
-                <button type="submit">Invia candidatura</button>
+                <button type="submit"
+                    class="cursor-pointer border border-sky-600 p-2 rounded-3xl text-sm hover:bg-sky-400">Invia
+                    candidatura</button>
             </form>
         </div>
     </div>
