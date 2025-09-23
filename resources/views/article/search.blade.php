b74aec543468422c85bb516c1000550d704304aa
peppe2412 <peppeterrazzano79@gmail.com>
2025-09-23 14:46:53 +0200

    ...

--- resources/views/article/search.blade.php
+++ resources/views/article/search.blade.php
@@ -10,7 +10,10 @@
             @forelse ($articles as $article)
                 <x-article-card :article="$article"/>
             @empty
-                <p class="text-center text-2xl">Sembra che non ci siano articoli corrispondenti alla ricerca</p>
+                <p class="text-center text-2xl mb-10">Sembra che non ci siano articoli corrispondenti alla ricerca</p>
+                <div class="flex justify-center">
+                    <a href="{{ route('article-index') }}" class="text-center text-lg bg-sky-300 p-[6px] rounded text-white hover:bg-sky-400">Sfoglia articoli</a>
+                </div>
             @endforelse
         </div>
     </div>
