a4138d415f180a7b897115b7887f34acf1ef1d5f
peppe2412 <peppeterrazzano79@gmail.com>
2025-09-27 19:46:06 +0200

    Responsive Article index

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -2,15 +2,14 @@
 
 
     <header class="flex justify-center py-16">
-        <h1 class="text-8xl title-article-index">Articoli</h1>
+        <h1 class="lg:text-8xl text-6xl  title-article-index">Articoli</h1>
     </header>
 
-    <div class="container py-5">
-        <div class="grid md:grid-cols-1 gap-3">
+    <div class="lg:container py-5">
+        <div class="lg:grid lg:grid-cols-1 lg:gap-3 p-4">
             @foreach ($articles as $article)
                 <x-article-card :article="$article" />
             @endforeach
-
         </div>
     </div>
 
