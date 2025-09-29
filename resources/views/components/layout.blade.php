dd99da77c702ea38b713ee49f086dc8408ec7e28
peppe2412 <peppeterrazzano79@gmail.com>
2025-09-29 14:36:03 +0200

    Aggiunta favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>The Post</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
 </head>
