3e9e11485fc56fc2ce2670d1b3912cce53274c49
peppe2412 <peppeterrazzano79@gmail.com>
2025-10-14 22:42:52 +0200

    Responsive article-search

--- resources/views/article/search.blade.php
+++ resources/views/article/search.blade.php
@@ -2,15 +2,15 @@
 
 
     <header class="flex justify-center py-16">
-        <h1 class="text-6xl">Articoli per {{ $query }}</h1>
+        <h1 class="lg:text-6xl md:text-4xl text-3xl">Articoli per {{ $query }}</h1>
     </header>
 
-    <div class="container py-5">
+    <div class="lg:container md:mx-auto lg:py-5">
         <div class="grid md:grid-cols-1 gap-3">
             @forelse ($articles as $article)
                 <x-article-card :article="$article"/>
             @empty
-                <p class="text-center text-2xl mb-10">Sembra che non ci siano articoli corrispondenti alla ricerca</p>
+                <p class="text-center lg:text-2xl text-xl lg:p-0 p-3 mb-10">Sembra che non ci siano articoli corrispondenti alla ricerca</p>
                 <div class="flex justify-center">
                     <a href="{{ route('article-index') }}" class="text-center text-lg bg-sky-300 p-[6px] rounded text-white hover:bg-sky-400">Sfoglia articoli</a>
                 </div>
