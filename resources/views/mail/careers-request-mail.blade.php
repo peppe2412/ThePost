a7db6215d41cb2a2ec055a35e17ae322e8aca9c8
peppe2412 <peppeterrazzano79@gmail.com>
2025-10-14 23:24:55 +0200

    Front-end careers request email

--- resources/views/mail/careers-request-mail.blade.php
+++ resources/views/mail/careers-request-mail.blade.php
@@ -4,12 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Richiesta</title>
+    <style>
+        body{
+            font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif ;
+        }
+        h3{
+            margin: 0;
+            margin-top: 15px;
+        }
+        p{
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
-    
     <h1>E' arrivata una richiesta di lavoro, per il ruolo di {{ $info['role'] }}</h1>
     <p>Ricevuta da {{ $info['email'] }}</p>
     <h3>Messaggio:</h3>
-    <p>{{$info['message']}}</p>
+    <p>{{ $info['message'] }}</p>    
 </body>
 </html>
+
+
+
+
+
