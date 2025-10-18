bc2419def8ddb4ce05615014bfc0b754b2f97817
peppe2412 <peppeterrazzano79@gmail.com>
2025-10-18 18:11:31 +0200

    Modificati message-span

--- resources/js/errorSpan.js
+++ resources/js/errorSpan.js
@@ -1,8 +1,6 @@
 let button_error_close = document.querySelector("#button-error-close");
-let auth_error_box = document.querySelector('.auth-error-box');
+let auth_error_box = document.querySelector(".auth-error-box");
 
-button_error_close.addEventListener('click', ()=>{
-    if(auth_error_box){
-        auth_error_box.classList.add('hidden')
-    }
-})
+button_error_close.addEventListener("click", () => {
+    auth_error_box.classList.add("hidden");
+});
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,14 @@
 
     @if (session('message'))
         <div class="flex justify-center py-8">
-            <div class="tw-message-span">
+            <div class="message-span relative">
+                <span class="text-green-400 absolute top-3 left-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </span>
                 {{ session('message') }}
             </div>
         </div>
@@ -12,7 +19,14 @@
 
     @if (request()->has('verified'))
         <div class="flex justify-center py-8">
-            <div class="tw-message-span">
+            <div class="message-span relative">
+                <span class="text-green-400 absolute top-3 left-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+                </span>
                 Verifica eseguita! Registrazione completata!
             </div>
         </div>
@@ -20,7 +34,7 @@
 
     @if (request()->has('logout'))
         <div class="flex justify-center py-8">
-            <div class="logout-span relative">
+            <div class="message-span relative">
                 <span class="text-green-400 absolute top-3 left-3">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="size-6">
